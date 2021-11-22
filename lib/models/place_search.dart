8aa93297de89685a04fd8323b2977749f4b5f244
eh1154 <elvis.hidalgo@unh.edu>
2021-11-22 05:00:36 +0000

    Upload New File

---
+++ lib/models/place_search.dart
@@ -0,0 +1,13 @@
+// Autocomplete class
+class PlaceSearch {
+  final String description; // name of location
+  final String placeId; // identifier
+
+  PlaceSearch({required this.description, required this.placeId});
+
+  // Scrapes data from json using string key arguements
+  factory PlaceSearch.fromJson(Map<String, dynamic> json) {
+    return PlaceSearch(
+        description: json['description'], placeId: json['place_id']);
+  }
+}
