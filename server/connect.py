a65efabcd09bd6adb1bed604cce2ee8cd07d5e39
dyk1001 <dyk1001@wildcats.unh.edu>
2022-03-29 18:17:05 -0400

    Added some stuff to try to run on a remote server

--- server/connect.py
+++ server/connect.py
@@ -3,4 +3,11 @@ from pymongo import MongoClient
 class Connect(object):
     @staticmethod    
     def get_connection():
-        return MongoClient("mongodb+srv://spaitrAdmin:spaitr@testcluster.dy8i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        # Running with Docker
+		# To view it in MongoDB Compass:
+		#  - Host: localhost
+		#  - Port: 7017
+		#  - Username: root
+		#  - Password: 123456
+		return MongoClient("mongodb://root:123456@mongodb/test_db?retryWrites=true&w=majority&authSource=admin")
+        #return MongoClient("mongodb+srv://spaitrAdmin:spaitr@testcluster.dy8i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
