1664510c0d314548b2127c3b120dcf8c39ac43d6
dyk1001 <dyk1001@wildcats.unh.edu>
2022-03-29 18:46:21 -0400

    another thing

--- server/connect.py
+++ server/connect.py
@@ -4,10 +4,11 @@ class Connect(object):
     @staticmethod    
     def get_connection():
         # Running with Docker
-		# To view it in MongoDB Compass:
-		#  - Host: localhost
-		#  - Port: 7017
-		#  - Username: root
-		#  - Password: 123456
-		return MongoClient("mongodb://root:123456@mongodb/test_db?retryWrites=true&w=majority&authSource=admin")
+        # To view it in MongoDB Compass:
+        #  - Host: localhost
+        #  - Port: 7017
+        #  - Username: root
+        #  - Password: 123456
+        return MongoClient("mongodb://root:123456@mongodb/test_db?retryWrites=true&w=majority&authSource=admin")
+        
         #return MongoClient("mongodb+srv://spaitrAdmin:spaitr@testcluster.dy8i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
